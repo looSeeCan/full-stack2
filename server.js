96efe4a418992c341a81dde89f3adb53cc8541b8
looSeeCan2 <nruabnrabkojthiabkuv@gmail.com>
2022-12-01 00:11:35 -0600

    Full Stack Developer Coding Challenge
    Finished the first video. Made a few changes. Added the buttons and changed the objects a bit in the server.js page

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+///when values are changed kill task and restart
+
 const express = require("express"), cors = require("cors"); /// create an express server
 
 const API_PORT = process.env.PORT || 5001; /// defining a port. I changed this from 5000 to 5001. The first time I did this tutorial it was set on 5000.
@@ -15,7 +17,11 @@ app.get("/api", (req, res) => { /// "/api" is a url. I think this can be any nam
 /// 2nd call. for quit
 app.get("/quit", (req, res) => {
     console.log("called quit");
-    res.send({result: "Good Bye"});
+    res.send({
+        result: "Ricki",
+        last: "Albright",
+        age: 36,
+    });
 })
 
 
