0f7501ab0835d7d7585111b21cadb52fc538b184
looSeeCan2 <nruabnrabkojthiabkuv@gmail.com>
2022-12-02 16:55:46 -0600

    friday night
    bucks game

---
+++ dbFiles/dbConfig.js
@@ -0,0 +1,15 @@
+const config = {
+    user: "CodingWithMyHomeLapTop",
+    password: "lucy",
+    server: "DESKTOP-B488TGH",
+    database: "Employee",
+    options: {
+        trustServerCertificate: true,
+        trustedConnection: false,
+        enableArithAbort: true,
+        instancename: "SQLEXPRESS"
+    },
+    PORT:1433
+}
+
+module.exports = config;
