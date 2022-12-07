0ed69d57759ec0884f336f3a6a0bd991c79a6f0f
looSeeCan2 <nruabnrabkojthiabkuv@gmail.com>
2022-12-06 23:09:13 -0600

    22:14
    on last vid

--- src/App.js
+++ src/App.js
@@ -5,33 +5,89 @@ import React, {useState} from "react";
 
 function App() {
 	const [returnedData, setReturnedData] = useState("");
+	/// be sure that the useState key matches the name in the input
+	const [employee, setEmployee] = useState({sso: 0, fullName: "", email: "", birthday: "", password: ""});
+	
+	const setInput = (e) => {
+		const {name, value} = e.target;
+		console.log(value, name);
+		// setEmployee({...employee , [name]: value});
 
-	// const a = dbOperation.getEmployees().then(res => {
-	// 	console.log(res.recordset);
-	// })
+		if(name === "sso") { /// if the input that we are in === sso, convert the value that is entered into a number
+			setEmployee(prevState => ({
+				...prevState,
+				[name]: parseInt(value)
+			}));
+			return; /// return early
+		}
+		setEmployee(prevState => ({ /// if not basically just keep the value. 
+			...prevState,
+			[name]: value
+		}));
+	}
 
-	const getData = async (url) => {/// this function has a parameter that accepts a url. 
-		/// the await fetch Gets the information (via the "url" parameter) in the app.get function in server.js
-		const newData = await fetch(url, {
-			method: "Get",
-			/// headers help us tell the response how we are gettting and accepting data
-			headers: { 
-				'content-type': 'application/json',
-				"Accept": "application/json"
-			}
-		})
-		.then(res => res.json())
+	const fetchData = async (url) => {/// this function has a parameter that accepts a url. 
+		console.log(employee);
+		
+		// /// the await fetch Gets the information (via the "url" parameter) in the app.get function in server.js
+		// const newData = await fetch(url, {
+		// 	method: "Get",
+		// 	/// headers help us tell the response how we are gettting and accepting data
+		// 	headers: { 
+		// 		'content-type': 'application/json',
+		// 		"Accept": "application/json"
+		// 	}
+		// })
+		// .then(res => res.json())
 
-		console.log(newData);
-		setReturnedData(newData);
+		// console.log(newData);
+		// setReturnedData(newData);
 	}
 
 	// getData("/api"); /// call the function ^. passes this argument which is the value in the app.get function in server.js
 
 	return (
 		<div className='App'>
-			<button onClick={() => getData("/api")}>Lucycan</button>
-			<button onClick={() => getData("/quit")}>Ricki</button>
+			<input 
+				type="number"
+				name='sso'
+				placeholder='SSO'
+				onChange={setInput}>
+			</input>
+
+			<input
+				name='fullName'
+				placeholder='Full Name'
+				onChange={setInput}>
+			</input>
+
+			<input
+				type="email"
+				name='email'
+				placeholder='email'
+				onChange={setInput}>
+			</input>
+
+			<input
+				type="date"
+				name="birthday"
+				placeholder='D.O.B'
+				onChange={setInput}>
+			</input>
+
+			<input
+				type="password"
+				name="password"
+				placeholder='Password'
+				onChange={setInput}>
+			</input>
+			<button onClick={() => fetchData("/api")}>Click</button>
+			<button onClick={() => fetchData("/quit")}>Create</button>
+			<p>SSO: {returnedData.sso}</p>
+			<p>Full Name: {returnedData.fullName}</p>
+			<p>Email: {returnedData.email}</p>
+			<p>D.O.B: {returnedData.birth}</p>
+			<p>Password: {returnedData.pwd}</p>
 		</div>
 	);
 }
